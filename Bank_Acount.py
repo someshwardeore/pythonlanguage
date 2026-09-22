173da9b7790694ea2c5d8d7aaddaba6e2a7f571a
someshwardeore <someshwardeore@gmail.com>
2026-09-22 22:31:53 +0530

    add: array

--- Bank_Acount.py
+++ Bank_Acount.py
@@ -17,8 +17,10 @@ class Bank_Acount:
     def Balance(self):
         print(f"total amount is : {self.__balance}")
 
+a=int(input("debit :"))
+b=int(input("credit :"))
 h1=Bank_Acount(5000,"someshwar")
-h1.debit(500)
-h1.credit(1000)
+h1.debit(a)
+h1.credit(b)
 h1.Balance()
           
